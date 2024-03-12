72f318c83ba112e888df977ef978c32767760ffd
Tokarzewski <bartlomiej.tokarzewski@gmail.com>
2024-03-12 21:37:39 +0100

    update folder name and add 2 files

--- python/iso_11855/methodology.py
+++ python/iso_11855/methodology.py
@@ -1,4 +1,4 @@
-import functions as f
+import iso_11855.functions as f
 from dataclasses import dataclass, field
 
 
--- python/iso_6946/example.py
+++ python/iso_6946/example.py
@@ -4,18 +4,20 @@ from functions import *
 Wood = Material(name="Wood", thickness=0.05, conductivity=0.9)
 Concrete = Material(name="Concrete", thickness=0.3, conductivity=1.0)
 ExternalWall = Construction(name="ExternalWall", materials=[Wood, Concrete])
+InternalFloor = Construction(name="InternalFloor", materials=[Wood, Concrete])
 
 boundary = "ext"
 direction = "horizontal"
 
-R_s_c = R_s_c(boundary=boundary, direction=direction)
+#R_s_c = R_s_c(boundary=boundary, direction=direction)
 
-U_test = Transmittance(
-    name="TExternalWall", construction=ExternalWall, direction="horizontal"
+U_EW = Transmittance(
+    name="ExternalWall", construction=ExternalWall, direction="horizontal"
+)
+U_IF = Transmittance(
+    name="InternalFloor", construction=InternalFloor, direction="upwards"
 )
 
 print("External Wall")
 print("R:", ExternalWall.R)
-print("U:", round(U_test.U, 2))
-print("R_se:", U_test.R_se)
-print("R_se_c:", R_s_c)
+print("R_se:", U_EW.R_se)
